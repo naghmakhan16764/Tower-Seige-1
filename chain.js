6378c8a3c1eb2e429fe52f089786d478b18aee25
naghmakhan16764 <74590340+naghmakhan16764@users.noreply.github.com>
2021-05-06 22:13:55 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,33 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length:20
+        }
+        this.pointB=pointB
+        this.chain = Matter.Constraint.create(options);
+        World.add(world,this.chain);
+    
+    }
+    display(){
+
+        if(this.chain.bodyA!==null)
+        {
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.pointB;
+        line(pointA.x,pointA.y,pointB.x,pointB.y)
+
+        }
+    
+    }
+ 
+    fly(){
+
+        this.chain.bodyA=null;
+
+    
+    }
+
+}
